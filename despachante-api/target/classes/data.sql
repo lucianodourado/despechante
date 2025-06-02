a39ad6eadaadca9ffb6fc87a0cefa78d2f92ba46
luciano dourado <luciano.dourado@mpt.mp.br>
2025-06-02 16:05:59 +0000

    compatibilização

--- despachante-api/target/classes/data.sql
+++ despachante-api/target/classes/data.sql
@@ -28,4 +28,43 @@ INSERT INTO TIPO_AGRUPAMENTO(ID_TIPO_AGRUPAMENTO, descricao) VALUES(7,'Gratifica
 INSERT INTO TIPO_AGRUPAMENTO(ID_TIPO_AGRUPAMENTO, descricao) VALUES(8,'Gratificação acumulação de ofício');
 INSERT INTO TIPO_AGRUPAMENTO(ID_TIPO_AGRUPAMENTO, descricao) VALUES(9,'Rubricas pagas em decorrência de local de trabalho (Ad. Atividade penosa, Ad. Insalubridade, Ad. Periculosidade etc)');
 
+INSERT INTO RACA(id_raca, descricao) VALUES (1,'Preta');
+INSERT INTO RACA(id_raca, descricao) VALUES (2,'Branca');
+INSERT INTO RACA(id_raca, descricao) VALUES (3,'Amarela');
+INSERT INTO RACA(id_raca, descricao) VALUES (4,'Parda');
+INSERT INTO RACA(id_raca, descricao) VALUES (5,'Indígena');
+INSERT INTO RACA(id_raca, descricao) VALUES (99,'Não desejo informar');
+
+INSERT INTO ESTADO_CIVIL(id_estado_civil, descricao) VALUES(1,'Solteiro');
+INSERT INTO ESTADO_CIVIL(id_estado_civil, descricao) VALUES(2,'Casado');
+INSERT INTO ESTADO_CIVIL(id_estado_civil, descricao) VALUES(3,'Viúvo');
+INSERT INTO ESTADO_CIVIL(id_estado_civil, descricao) VALUES(4,'Separado');
+INSERT INTO ESTADO_CIVIL(id_estado_civil, descricao) VALUES(5,'Divorciado');
+INSERT INTO ESTADO_CIVIL(id_estado_civil, descricao) VALUES(6,'União Estável');
+INSERT INTO ESTADO_CIVIL(id_estado_civil, descricao) VALUES(99,'Outros');
+
+INSERT INTO GRAU_INSTRUCAO(id_grau_instrucao, descricao) VALUES(1,'ANALFABETO');
+INSERT INTO GRAU_INSTRUCAO(id_grau_instrucao, descricao) VALUES(2,'ATÉ O 5º ANO INCOMPLETO DO ENSINO FUNDAMENTAL (ANTIGA 4ª SÉRIE');
+INSERT INTO GRAU_INSTRUCAO(id_grau_instrucao, descricao) VALUES(3,'5º ANO COMPLETO DO ENSINO FUNDAMENTAL');
+INSERT INTO GRAU_INSTRUCAO(id_grau_instrucao, descricao) VALUES(4,'DO 6º AO 9º ANO DO ENSINO FUNDAMENTAL (ANTIGA 5ª À 8ª SÉRIE)');
+INSERT INTO GRAU_INSTRUCAO(id_grau_instrucao, descricao) VALUES(5,'ENSINO FUNDAMENTAL COMPLETO');
+INSERT INTO GRAU_INSTRUCAO(id_grau_instrucao, descricao) VALUES(6,'ENSINO MÉDIO INCOMPLETO');
+INSERT INTO GRAU_INSTRUCAO(id_grau_instrucao, descricao) VALUES(7,'ENSINO MÉDIO COMPLETO');
+INSERT INTO GRAU_INSTRUCAO(id_grau_instrucao, descricao) VALUES(8,'EDUCAÇÃO SUPERIOR INCOMPLETA');
+INSERT INTO GRAU_INSTRUCAO(id_grau_instrucao, descricao) VALUES(9,'EDUCAÇÃO SUPERIOR COMPLETA');
+INSERT INTO GRAU_INSTRUCAO(id_grau_instrucao, descricao) VALUES(10,'ESPECIALIZAÇÃO');
+INSERT INTO GRAU_INSTRUCAO(id_grau_instrucao, descricao) VALUES(11,'MESTRADO');
+INSERT INTO GRAU_INSTRUCAO(id_grau_instrucao, descricao) VALUES(12,'DOUTORADO');
+
+INSERT INTO IDENTIDADE_GENERO(id_identidade_genero, descricao) VALUES(1,'Homem Transgênero');
+INSERT INTO IDENTIDADE_GENERO(id_identidade_genero, descricao) VALUES(2,'Mulher Transgênero');
+
+INSERT INTO TIPO_TREINAMENTO(id_tipo_treinamento, descricao) VALUES(1, 'Institucional');
+INSERT INTO TIPO_TREINAMENTO(id_tipo_treinamento, descricao) VALUES(2, 'para Particular');
+
+INSERT INTO SEXO(id_sexo, descricao) VALUES(1, 'Masculino');
+INSERT INTO SEXO(id_sexo, descricao) VALUES(2, 'Feminino');
+
+
+
 
